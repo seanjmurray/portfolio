953080748332c782eff234df41156abd96ded491
Sean Murray <sean.j.murray1@gmail.com>
2020-07-01 13:42:41 -0700

    Move all components to App and update README

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,7 +4,7 @@ export default function Contact(){
     return (
       <section id="contact" className="card">
         <h1>Get ahold of me</h1>
-        <p>If you have any questions about my work feel free to email me, connect with me on Linkedin, or make a pull on any of my repo's. </p>
+        <p className="col-9">If you have any questions about my work feel free to email me, connect with me on Linkedin, or make a pull on any of my repo's. </p>
         <br/>
         <div className="social">
         <a href="mailto:sean@seanjmurray.tech" className="email"><i className="far fa-envelope"></i> Email me</a>
--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Header from './components/header';
-import Hero from './components/hero';
-import About from './components/about';
-import Projects from './components/projects';
-import Skills from './components/competencies';
-import Contact from './components/contact';
-import Footer from './components/footer';
+import App from './app';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-  <section>
-    <Header />
-    <Hero />
-    <main>
-    <About />
-    <Projects />
-    <Skills />
-    <Contact />
-    </main>
-    <Footer />
-  </section>
+  <App />
   ,document.getElementById('root')
 );
 
