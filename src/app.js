96cf6c047b372ffaadf9fa8f361f6897a187cdaa
Sean Murray <sean.j.murray1@gmail.com>
2021-05-28 20:13:02 +0200

    Add lazy loading to all components and start to build loading component'

--- src/app.js
+++ src/app.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import Header from './components/header';
-import Hero from './components/hero';
-import About from './components/about';
-import Projects from './components/projects';
-import Resume from './components/resume';
-import Skills from './components/competencies';
-import Contact from './components/contact';
-import Footer from './components/footer';
+import React, { Suspense, lazy } from 'react'; 
+import Loading from './components/loading'
+// Lazy loading components 
+const Header = lazy(() => import('./components/header'))
+const Hero = lazy(() => import('./components/hero'))
+const About = lazy(() => import('./components/about'))
+const Projects = lazy(() => import('./components/projects'))
+const Resume = lazy(() => import('./components/resume'))
+const Skills = lazy(() => import('./components/competencies'))
+const Contact = lazy(() => import('./components/contact'))
+const Footer = lazy(() => import('./components/footer'))
+
 
 const App = () =>  {
   return(
+    <Suspense fallback={<Loading></Loading>}>
     <section>
       <Header />
       <Hero />
@@ -22,6 +26,7 @@ const App = () =>  {
     </main>
       <Footer />
     </section>
+    </Suspense>
     )
 }
 export default App;
---
+++ src/components/loading.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Loading = () => {
+    return (
+        <section className='loading'>
+            <h1>Loading...</h1>
+        </section>
+    )
+}
+export default Loading;
